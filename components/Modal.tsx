d4c2663f5aa603ec4a92449e56cdaeb738dfe69d
Arik Alexandrov <arikxl@gmail.com>
2023-07-19 00:19:09 +0300

    upload img2

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Fragment, useRef } from 'react'
+import { FormEvent, Fragment, useRef } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { useModalStore } from '@/store/ModalStore';
 import { useBoardStore } from '@/store/BoardStore';
@@ -23,11 +23,21 @@ function Modal() {
         state.setImage,
     ])
 
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!newTaskInput) return;
+
+
+        setImage(null);
+        closeModal();
+    }
 
     return (
         // Use the `Transition` component at the root level
         <Transition appear show={isOpen} as={Fragment}>
-            <Dialog as="form" onClose={closeModal} className='relative z-10'>
+            <Dialog as="form" onClose={closeModal} className='relative z-10'
+                onSubmit={(e) => handleSubmit(e)}
+            >
 
                 <Transition.Child
                     as={Fragment}
@@ -94,6 +104,17 @@ function Modal() {
                                             setImage(e.target.files![0])
                                         }}
                                     />
+                                </div>
+                                <div>
+                                    <button className='inline-flex justify-center rounded-md border
+                                        border-transparent bg-blue-100 px-4 py-2 text-sm font-medium 
+                                        text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2
+                                        focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:bg-gray-100
+                                        disabled:text-gray-300 disabled:cursor-not-allowed mt-2'
+                                        type='submit' disabled={!newTaskInput}
+                                    >
+                                        Add Todo
+                                    </button>
                                 </div>
                             </Dialog.Panel>
                         </Transition.Child>
